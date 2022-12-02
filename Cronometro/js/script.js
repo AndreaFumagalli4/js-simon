a4a1490b4bde4a25bde22a4d5442e4cf9c0bed36
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-02 17:53:50 +0100

    ad add event listener

---
+++ Cronometro/js/script.js
@@ -0,0 +1,19 @@
+const tempo = document.getElementById('tempo');
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const reset = document.getElementById('reset');
+
+let time = 0;
+
+start.addEventListener('click', function(){
+    
+});
+
+pause.addEventListener('click', function(){
+
+});
+
+reset.addEventListener('click', function(){
+
+});
+
