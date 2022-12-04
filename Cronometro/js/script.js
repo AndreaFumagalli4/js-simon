661a070e70a4c0379ca33c967618e945d5065a8c
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-04 14:20:07 +0100

    add timer comments

--- Cronometro/js/script.js
+++ Cronometro/js/script.js
@@ -3,17 +3,29 @@ const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
-let time = 0;
 
-start.addEventListener('click', function(){
+
+start.addEventListener( 'click', function() {
+
+    let isCount = true;
+    let time = 0;
     
+    pause.addEventListener('click', function(){
+    
+    });
+    
+    reset.addEventListener('click', function(){
+        
+    });
 });
 
-pause.addEventListener('click', function(){
+/**
+ *  1. - Quando l'utente clicca start parte il conteggio dei secondi: set interval - ogni secondo alla variabile time si somma 1;
+ *  2. - Quando clicca su pause il conteggio si ferma: 
+ *  2.1 - SE clicca ancora su start ==> il conteggio riparte; 
+ *  2.2 - SE clicca su reset ==> il conteggio si azzera;
+ * 3. - Se l'utente, dopo avere azzerato, clicca su start il conteggio riparte. 
+ */
 
-});
 
-reset.addEventListener('click', function(){
-
-});
 
